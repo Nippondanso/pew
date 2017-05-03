5fed34c556681aac77e9b7d523bf71f4fee73cd4
MaksymKopaiev <mkopaev@gmail.com>
2017-05-03 14:43:26 +0300

    - Создал класс Figure, от него унаследовал функционал классами HorizontalLine VerticalLine, - отрисовал рамочку, - задал стандартный размер окна.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,33 +11,16 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            //p1.Draw();
+            p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            //p1.Draw();
+            Point p2 = new Point(5, 4, '#');
+            p2.Draw();
 
-            List<char> znakList = new List<char>();
-            znakList.Add('*');
-            znakList.Add('@');
-            znakList.Add(':');
-            znakList.Add('\'');
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Draw();
 
-            //foreach(char i in znakList)
-            //{
-            //    Console.WriteLine(i);
-            //}
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            foreach(Point p in pList)
-            {
-                p.Draw();
-
-            }
-            
-
+            VerticalLine line2 = new VerticalLine(4, 9, 4, '+');
+            line2.Draw();
 
             Console.ReadKey();
         }
